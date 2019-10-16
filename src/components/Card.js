dc3b86af145679bc7ccaeff2b667cb380c43e8d7
Evgeny Shupikov <shupik@sedi.ru>
2019-10-16 16:33:06 +0400

    add state to Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ButtonGroupTimeInterval from './ButtonGroupTimeInterval';
 import { TIME_INTERVAL, BOND_DATA_TYPE } from '../store/bonds/types';
 import DropDownBondDataType from './DropDownBondDataType';
@@ -6,22 +6,28 @@ import Header from './Header';
 
 
 function Card() {
+
+  const [chartInterval, setChartInterval] = useState(TIME_INTERVAL.Month);
+  const [chartDataType, setChartDataType] = useState(BOND_DATA_TYPE.Price);
+
   return (
     <div>
 
       {/* header */}
       <Header isin={'US67021BAE92'}
-        shortName={'NII CAPITAL'} 
+        shortName={'NII CAPITAL'}
         fillName={'NII CAPITAL CORP, Telecommunications, NR'}
         yieldValue={7.625}
-        price={21} 
-        currency={'USD'} 
+        price={21}
+        currency={'USD'}
         date={new Date()} />
 
       <hr />
 
       {/* buttons */}
-      <ButtonGroupTimeInterval selectedValue={TIME_INTERVAL.Month} />
+      <ButtonGroupTimeInterval
+        selectedValue={chartInterval}
+        onClickButton={interval => setChartInterval(interval)} />
 
       <div className="position-relative">
 
@@ -31,7 +37,9 @@ function Card() {
         {/* data type */}
         <div className="position-absolute fixed-bottom">
           <div className="float-right mr-3 mb-3">
-            <DropDownBondDataType menuAlign="right" selectedValue={BOND_DATA_TYPE.Price} />
+            <DropDownBondDataType menuAlign="right"
+              selectedValue={chartDataType}
+              onClickItem={dataType => setChartDataType(dataType)} />
           </div>
         </div>
       </div>
